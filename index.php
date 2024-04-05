d0a80a1e5031baf1cd832fcc60423b23ab266e18
burillu <burillu90@hotmail.it>
2024-04-05 17:00:07 +0200

    snack 3 not solved

--- index.php
+++ index.php
@@ -74,4 +74,132 @@ For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should r
     
       return array($count_positive, $sum_negative);
   }
+
+
+/* 
+In this kata we are going to mimic a software versioning system.
+
+You have to implement a VersionManager class.
+
+It should accept an optional parameter that represents the initial version. The input will be in one of the following formats: "{MAJOR}", "{MAJOR}.{MINOR}", or "{MAJOR}.{MINOR}.{PATCH}". More values may be provided after PATCH but they should be ignored. If these 3 parts are not decimal values, an exception with the message "Error occured while parsing version!" should be thrown. If the initial version is not provided or is an empty string, use "0.0.1" by default.
+
+This class should support the following methods, all of which should be chainable (except release):
+
+major() - increase MAJOR by 1, set MINOR and PATCH to 0
+minor() - increase MINOR by 1, set PATCH to 0
+patch() - increase PATCH by 1
+rollback() - return the MAJOR, MINOR, and PATCH to their values before the previous major/minor/patch call, or throw an exception with the message "Cannot rollback!" if there's no version to roll back to. Multiple calls to rollback() should be possible and restore the version history
+release() - return a string in the format "{MAJOR}.{MINOR}.{PATCH}"
+
+May the binary force be with you!
+*/
+
+class VersionManager 
+{
+  public $major = 0;
+  public $minor = 0;
+  public $patch = 0;
+  public $history = [];
+  //public $fail = 'Error occured while parsing version!';
+  
+  public function __construct($string = '0.0.1')
+  {
+    var_dump($string);
+    if(empty($string)){
+        $string = '0.0.1';
+        //throw new Exception('Error occured while parsing version!');
+    }
+    //var_dump($string);
+   //var_dump($string);
+    for ($i=0; $i < strlen($string) ; $i++) {
+        if($string[$i] != '.' && !is_int((int) $string[$i])){
+          throw new Exception('Error occured while parsing version!');
+          
+        } 
+      
+        $this->major = (int) $string[0];
+        if($i == 2){
+           $this->minor =  (int) $string[$i] ;
+        } 
+        if($i == 4){
+            $this->patch = (int) $string[$i]; 
+        }
+        //$this->history[]= $this->release();        
+    }
+
+  }
+  
+  public function major()
+  {
+    var_dump($this->release());
+    $this->history[] = $this->release();
+    $this->major++;
+    $this->minor = 0;
+    $this->patch = 0;
+    
+    
+  }
+   public function minor()
+  {
+    $this->history[] = $this->release();
+    $this->minor++;
+    $this->patch = 0;
+    
+  }
+   public function patch()
+  {
+    $this->history[] = $this->release();
+    $this->patch++;
+    
+  }
+  public function rollback()
+  {
+    $element_history = count($this->history);
+    if( $element_history < 1){
+       return throw new Exception('Cannot rollback!');
+    }
+    //prendo l'ultimo elemento
+    $last_element = $this->history[$element_history - 1];
+    //assegno i precedenti valori contenuti nello storico
+    $this->major = $last_element[0];
+    $this->minor = $last_element[2];
+    $this->patch = $last_element[4];
+    array_pop($this->history);
+
+  }
+  public function release()
+  {
+    return "$this->major.$this->minor.$this->patch";
+  }
+}
+
+
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php  $version = new VersionManager('6.9.5');  
+    //var_dump("Ciucciala")
+    var_dump($version);
+    //var_dump($version->history[0]);
+
+    $version->major();
+
+    var_dump($version);
+
+    $version->rollback();
+
+    var_dump($version);
+    
+    ;
+    ?>
+</body>
+</html>
