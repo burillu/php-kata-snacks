cde0aa3db900587270cac751e824d2f32265fb17
burillu <burillu90@hotmail.it>
2024-04-05 18:46:34 +0200

    snack 5 near the solution

--- index.php
+++ index.php
@@ -194,9 +194,38 @@ function replace_dots(string $str): string {
   }
 
 
+/*
+Determine if the poker hand is flush, meaning if the five cards are of the same suit.
 
+Your function will be passed a list/array of 5 strings, each representing a poker card in the format "5H" (5 of hearts), meaning the value of the card followed by the initial of its suit (Hearts, Spades, Diamonds or Clubs). No jokers included.
 
+Your function should return true if the hand is a flush, false otherwise.
 
+The possible card values are 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
+
+Examples
+["AS", "3S", "9S", "KS", "4S"]  ==> true
+
+["AD", "4S", "7H", "KS", "10S"] ==> false
+*/
+
+function checkIfFlush($cards)
+{
+  var_dump($cards);
+  $cards_values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+  $suits = [ 'H','S','D','C'];
+  $user_cards_suits = [];
+  foreach($cards as $card){
+    $user_cards_suits[]= $card[-1];
+  }
+  $different_suits = array_diff($suits, $user_cards_suits);
+  var_dump($different_suits);
+  if(!empty($different_suits)){
+    return true;
+  }else{
+    return false;
+    }
+}
 
 
 
